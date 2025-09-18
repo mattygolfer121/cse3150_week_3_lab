f190cd99d949b55bbe19cfa60c301b555e68fe4e
Matthew Jon <matty_jones@Matthews-MacBook-Air.local>
2025-09-18 01:32:02 -0400

    starter code

---
+++ src/logger.cpp
@@ -0,0 +1,71 @@
+#include "logger.h"
+#include <string>
+#include <iostream>
+
+// ======================= TODOs =======================
+
+void addReading(const std::string& label, double value,
+                std::string labels[], double values[],
+                int& size, int capacity) {
+    // TODO: throw "Full" if size == capacity, else insert and ++size
+	if (size == capacity){
+		throw std::string("Full");
+	} else {
+		labels[size] = label;
+		values[size] = value;
+		size++;
+	}
+}
+
+void updateValue(double* valuePtr, double newValue) {
+    // TODO: write through pointer
+	*valuePtr = newValue;
+}
+
+void printReading(const std::string& label, const double& value) {
+    // TODO: pretty-print one reading
+	std::cout << label << ": " << value << std::endl;
+}
+
+double average(const double values[], int size) {
+    // TODO: throw "Empty" if size==0, else compute average
+	if (size == 0){
+		throw std::string("Empty");
+	} else {
+		double sum = 0.0;
+		for (int i = 0; i < size; i++) {
+			sum += values[i];
+		}
+		return sum / size;
+	}
+}
+
+double minValue(const double values[], int size) {
+    // TODO: throw "Empty" if size==0, else compute min
+	if (size == 0) {
+		throw std::string("Empty");
+	} else {
+		double min = 999999999999.99;
+		for (int i = 0; i < size; i++) {
+			if (values[i] < min) {
+				min = values[i];
+			}
+		}
+		return min;
+	}
+}
+
+double maxValue(const double values[], int size) {
+    // TODO: throw "Empty" if size==0, else compute max
+    if (size == 0) {
+		throw std::string("Empty");
+	} else {
+		double max = 0.0;
+		for (int i = 0; i < size; i++) {
+			if (max < values[i]){
+				max = values[i];
+			}
+		}
+		return max;		
+	}
+}
